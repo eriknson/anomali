ac60d51cfd4e4bce2541fbbc4827a5707382d87f
eriknson <eriks@mail.se>
2020-04-15 15:38:35 +0200

    Imported our data and config

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,70 +1,80 @@
-import React, { Component } from 'react';
-import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer';
+// Copyright (c) 2018 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer';
 import KeplerGl from 'kepler.gl';
+
+// Kepler.gl actions
 import { addDataToMap } from 'kepler.gl/actions';
+// Kepler.gl Data processing APIs
 import Processors from 'kepler.gl/processors';
 
-import ulEdges from '../../data/ul-data-ver1.csv.js';
-import ulConfig from '../../data/ul-config';
+// Kepler.gl Schema APIs
+import KeplerGlSchema from 'kepler.gl/schemas';
+
+// Sample data
+import nycTrips from '../../data/ul-data-ver1.csv.js';
+import nycConfig from '../../data/ul-config';
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiZXJpa25zb24iLCJhIjoiY2pqanh2ZWYxNGE0ZjN3bzM4M3Azdzl6cSJ9.UO94WxxiSiHrAaztMgy_Yg'; // eslint-disable-line
 
-const data = Processors.processCsvData(ulEdges);
-const dataset = {
-  data,
-  info: {
-    // Kopplar den att detta ID är till datan?
-    id: 'fin7fpx0d',
-  },
-};
-
-const sampleTripData = {
-  fields: [
-    { name: 'tpep_pickup_datetime', format: 'YYYY-M-D H:m:s', type: 'timestamp' },
-    { name: 'pickup_longitude', format: '', type: 'real' },
-    { name: 'pickup_latitude', format: '', type: 'real' },
-  ],
-  rows: [
-    ['2015-01-15 19:05:39 +00:00', -73.99389648, 40.75011063],
-    ['2015-01-15 19:05:39 +00:00', -73.97642517, 40.73981094],
-    ['2015-01-15 19:05:40 +00:00', -73.96870422, 40.75424576],
-  ],
-};
-
-const sampleConfig = {
-  visState: {
-    filters: [
-      {
-        id: 'me',
-        dataId: 'test_trip_data',
-        name: 'tpep_pickup_datetime',
-        type: 'timeRange',
-        enlarged: true,
-      },
-    ],
-  },
-};
-
-class Map extends Component {
+class App extends Component {
   componentDidMount() {
-    console.log('Component did mount!');
-  }
-  /*addDataToMap({
-      datasets: dataset,
-      option: {
-        centerMap: true,
-        readOnly: false,
+    // Use processCsvData helper to convert csv file into kepler.gl structure {fields, rows}
+    const data = Processors.processCsvData(nycTrips);
+    // Create dataset structure
+    const dataset = {
+      data,
+      info: {
+        // this is used to match the dataId defined in nyc-config.json. For more details see API documentation.
+        // It is paramount that this id matches your configuration otherwise the configuration file will be ignored.
+        id: 'fin7fpx0d',
       },
-      config: ulConfig
-    });*/
+    };
+    // addDataToMap action to inject dataset into kepler.gl instance
+    this.props.dispatch(addDataToMap({ datasets: dataset, config: nycConfig }));
+  }
+
+  // This method is used as reference to show how to export the current kepler.gl instance configuration
+  // Once exported the configuration can be imported using parseSavedConfig or load method from KeplerGlSchema
+  getMapConfig() {
+    // retrieve kepler.gl store
+    const { keplerGl } = this.props;
+    // retrieve current kepler.gl instance store
+    const { map } = keplerGl;
+
+    // create the config object
+    return KeplerGlSchema.getConfigToSave(map);
+  }
+
+  // This method is used as reference to show how to export the current kepler.gl instance configuration
+  // Once exported the configuration can be imported using parseSavedConfig or load method from KeplerGlSchema
 
   render() {
     return (
       <div style={{ position: 'relative', width: '100%', height: '100%', minHeight: '70vh' }}>
         <AutoSizer>
           {({ height, width }) => (
-            <KeplerGl appName='anomali' mapboxApiAccessToken={MAPBOX_TOKEN} id='map' width={width} height={height} />
+            <KeplerGl mapboxApiAccessToken={MAPBOX_TOKEN} id='map' width={width} height={height} />
           )}
         </AutoSizer>
       </div>
@@ -72,4 +82,7 @@ class Map extends Component {
   }
 }
 
-export default Map;
+const mapStateToProps = (state) => state;
+const dispatchToProps = (dispatch) => ({ dispatch });
+
+export default connect(mapStateToProps, dispatchToProps)(App);
