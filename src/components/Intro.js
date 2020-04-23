ab09203c27f99ed9939e689f24746826eb2f5e7c
eriknson <eriks@mail.se>
2020-04-23 09:52:35 +0200

    .

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -19,23 +19,25 @@ class Intro extends Component {
             </strong>
           </p>
           <p>
-            For 20 weeks, we have had the honor to work with the Swedish Public
-            Transport Data Lab (a.k.a.{" "}
+            For 20 weeks, we have had the honor to work with Samtrafiken and the
+            Swedish Public Transport Data Lab (a.k.a.{" "}
             <a href="https://www.ri.se/sv/vad-vi-gor/projekt/koda">
               <u>KoDa</u>
             </a>
-            ). As a part of our project, we are developing a proof of concept to
-            assess weak spots (in terms of time table deviations) using graph
-            theory in the Uppsala Transit Network (UL)
+            ). As a part of the thesis project, we are developing a proof of
+            concept tool to assess weak spots (in terms of time table
+            deviations) using graph theory.
           </p>
           <p>
             More specifically, we have developed a pipeline that extracts,
-            aggregates and analyzes 3.5M data points from January 2020. The
-            result is an index describing how likely each node-pair (edge) in
-            the network is to deviate from schedule (the lines). Then, in order
-            to identify the most vulnerable part(s) / extension(s) in the
-            network, we assess them using the MINCUT-MAXFLOW algorithm (the
-            arcs). And here's our attempt to visualize this.
+            aggregates and analyzes 3.5M data points from the Uppsala Transit
+            Network (UL). Primarly, the result is an index describing how likely
+            each node-pair (edge) in the network is to deviate from schedule.
+            Secondly, in order to identify the most vulnerable part(s) /
+            extension(s) in the network, we assess them using the MINCUT-MAXFLOW
+            algorithm. And here's our attempt to visualize this. See the diagram
+            legend by clicking on the second button in the top-right corner of
+            the map.
           </p>
           <p>
             Feel to free to send{" "}
